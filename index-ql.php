fcdb45467ffc9299e69e06d1e573d209306efd91
thanhhuu09 <58838162+thanhhuu09@users.noreply.github.com>
2022-01-03 11:54:13 +0700

    ok

--- index-ql.php
+++ index-ql.php
@@ -39,36 +39,6 @@
     </div>
 </div> -->
 
-<nav class="navbar navbar-expand-lg border-bottom border-gray">
-    <div class="navbar-child order-0">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <span id="pointer" class="pointer" onclick="openNav()">&#9776;</span>
-            </li>
-            <li class="nav-item department">
-                <a>Phòng Ban Kiểm Thử Phần Mềm<br /></a>
-            </li>
-        </ul>
-    </div>
-    <ul class="navbar-nav mx-auto">
-        <li class="nav-item"><a class="nav-link" href="index.html"> Tình trạng công việc </a> </li>
-        <li class="nav-item"><a class="nav-link" href="index-ql.html"> Quản lý công việc </a></li>
-        <li class="nav-item"><a class="nav-link" href="index-ql.html"> Danh sách nghỉ phép </a></li>
-        <li class="nav-item"><a class="nav-link" href="#"> Quản lý nhân viên </a></li>
-    </ul>
-    
-    <div class="navbar-child mx-auto">
-        <div id ="user-info" >
-            <img class="avatar" src="images/male.svg">
-            <span class = "topbar_name">SangSinh21</span>
-            <div class="navbar-user-list">
-                <a class = "navbar-user-item" href="profile.html">Tài khoản của tôi</a>
-                <a class = "navbar-user-item" href="#">Nhắn tin</a>
-                <a class = "navbar-user-item" href="#">Đăng xuất</a>
-            </div>
-        </div>
-    </div>
-</nav>
 
 <!-- <nav class="navbar navbar-expand-lg border-bottom border-gray">
     <div class="navbar-child order-0">
@@ -82,13 +52,15 @@
         </ul>
     </div>
     <ul class="navbar-nav mx-auto">
-        <li class="nav-item"><a class="nav-link" href="index.html"> Tình trạng công việc </a> </li>
-        <li class="nav-item"><a class="nav-link" href="index-ql.html"> Quản lý công việc </a></li>
-        <li class="nav-item"><a class="nav-link" href="index-ql.html"> Danh sách nghỉ phép </a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php"> Tình trạng công việc </a> </li>
+        <li class="nav-item"><a class="nav-link" href="index-ql.php"> Quản lý công việc </a></li>
+        <li class="nav-item"><a class="nav-link" href="index-ql.php"> Danh sách nghỉ phép </a></li>
         <li class="nav-item"><a class="nav-link" href="#"> Quản lý nhân viên </a></li>
     </ul>
 </nav> -->
-
+<?php
+    require_once ('./Include/NavbarQL.php');
+?>
 <div class="container-fluid align-items-center">
     <div class="background background-ql col-md-8 col-sm-10"></div>
 </div>
